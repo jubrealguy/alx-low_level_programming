dec963b118a92a87a636006b8c6bd1d06188ae76
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-06-30 09:00:03 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,12 @@
 
      * main - Entry point
      *
-     *  *
-     *
-     *   * Return: Always 0 (Success)
-     *
-     *    */
+     * Return: Always 0 (Success)
+     */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
