9ad1b770b888597e486cd19069a4fd451d74404b
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-07-01 07:00:19 -0700

    three

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		printf("%c", letter);
+	return (0);
+}
