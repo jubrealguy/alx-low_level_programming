18596c8f98515267011bc053f1803c242f8f1495
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-07-01 14:08:46 -0700

    three

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (small_letter = 'a'; small_letter <= 'z'; small_letter++)
 		putchar(small_letter);
-	for (capital_letter = 'A'; capital_letter <= 'Z'; capital_letter++)
+	for (capital_letter = 0; capital_letter <= 'Z'; capital_letter++)
 		putchar(capital_letter);
 	return (0);
 }
