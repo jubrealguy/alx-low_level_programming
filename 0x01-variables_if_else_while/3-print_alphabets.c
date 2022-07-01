e907a909b82ece5f487c4b9a0f145bb823be8a17
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-07-01 13:53:31 -0700

    three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char small_letter, capital_letter;
+
+	for (small_letter = 'a'; small_letter <= 'z'; small_letter++)
+		putchar(small_letter);
+	for (capital_letter = 'A'; capital_letter <= 'Z'; capital_letter++)
+		putchar(capital_letter);
+	return (0);
+}
