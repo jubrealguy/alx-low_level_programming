f97839edc9a49f3c2783a9be21840bad0a2cd4fb
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-07-01 16:09:38 -0700

    eight

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int a;
 	char b;
-	
-	for (a = 0; a <= 9; a++)
-		putchar(a);
+
+	for (a = 0; a < 10; a++)
+		putchar((a % 10) + '0');
 	for (b = 'a'; b <= 'e'; b++)
 		putchar(b);
 	putchar('\n');
 	return (0);
-}	
+}
