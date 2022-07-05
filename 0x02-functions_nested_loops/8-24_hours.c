dd464aeb5e6eb54a1715de36698be4049472870a
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-07-05 15:39:36 -0700

    task eight

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Description: prints minute 0:00 - 23:59
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int mins = 0;
+
+	while (hours < 24)
+	{
+		while (mins < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (mins / 10));
+			_putchar('0' + (mins % 10));
+			_putchar('\n');
+			mins++;
+		}
+		mins = 0;
+		hours++;
+	}
+}
