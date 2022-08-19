0adcd9597bbb308149d270ab2789cae5e3ee6170
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-08-19 15:24:48 -0700

    combination of 2-digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 	{
 		for (b = a + 1; b < 10; b++)
 		{
-			putchar((a % 10) + '0');
-			putchar((b % 10) + '0');
+			putchar(a + '0');
+			putchar(b + '0');
 
 			if (a == 8 && b == 9)
 				continue;
