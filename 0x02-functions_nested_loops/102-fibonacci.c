7815b28cb02a7d3c418008074a1d7c5033ea4361
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-08-28 07:56:22 -0700

    fibonacci series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,8 +6,8 @@
 
 int main(void)
 {
-	long unsigned int a = 1;
-	long unsigned int b = 2;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
 	int i;
 
 	printf("%lu, %lu, ", a, b);
@@ -26,36 +26,3 @@ int main(void)
 	printf("\n");
 	return (0);
 }
-
-/**
-int main(void)
-{
-	int counter;
-	int countto = 50;
-	long a = 1;
-	long b = 2;
-
-	for (counter = 1; counter <= (countto / 2); counter++)
-	{
-		if (counter < (countto / 2))
-		{
-			printf("%li, %li, ", a, b);
-			a += b;
-			b += a;
-		}
-		else if (counter == (countto / 2))
-		{
-			printf("%li, %li", a, b);
-			a += b;
-			b += a;
-		}
-	}
-	if (countto % 2 == 1)
-	{
-		printf("%li", a);
-	}
-	printf("\n");
-	return (0);
-}
-
-**/
