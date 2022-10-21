e44083e9ff2a79641dbaf63b0137dd48a6dbfb25
Jubreel Adebayo <whizkidjubrealguy@gmail.com>
2022-10-21 13:12:10 -0700

    worked on the malloc line

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *arr = malloc(size * sizeof(hash_table_t));
+	hash_table_t *arr = malloc(sizeof(hash_table_t));
 
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
+
 
 	return (arr);
 }
