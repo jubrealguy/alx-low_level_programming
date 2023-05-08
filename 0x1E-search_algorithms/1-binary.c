ed958a721c57981a37549da2b98c0a0af1f27303
jubrealguy <whizkidjubrealguy@gmail.com>
2023-05-08 12:17:02 +0100

    search algorithm with binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,35 +1,58 @@
 #include <stdio.h>
-#include <string.h>
 #include "search_algos.h"
+
+/**
+ * print_search - prints the array searched
+ * @array: array to be printed
+ * @x: starting index
+ * @y: ending index
+ * Return: Return on success
+ */
+
+int print_search(int *array, int x, int y)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = x; i <= y; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+	return (0);
+}
+
+/**
+ * binary_search - searching an array for an index in a binary search algorithm
+ * @array: Array to be searched
+ * @size: Length of the array
+ * @value: Value to be searched for
+ * Return: Return index and -1 on fail
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0, j = 0;
-	int *new_arr = NULL;
+	int left_index = 0;
+	int right_index = size - 1;
 
-	while (i <= size)
+	while (left_index <= right_index)
 	{
-		int mid = i + (size - i) / 2;
-		if (array[mid] == value)
+		int mid_index = left_index + (right_index - left_index) / 2;
+
+		print_search(array, left_index, right_index);
+
+		if (array[mid_index] == value)
 		{
-			return mid;
+			return (mid_index);
 		}
-		else if (array[mid] < value)
+		else if (array[mid_index] < value)
 		{
-			i = mid + 1;
+			left_index = mid_index + 1;
 		}
 		else
 		{
-			size = mid - 1;
+			right_index = mid_index - 1;
 		}
-		new_arr[i] = array[i];
 	}
-
-	for (j = 0; i < size; j++)
-	{
-		printf("Searching in array: ");
-		printf("%d, ", new_arr[j]);
-	}
-	printf("\n");
 	return (-1);
 }
