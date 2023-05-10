f8e288561b5535a821eb42512d7e4b583b3a83f9
jubrealguy <whizkidjubrealguy@gmail.com>
2023-05-10 19:30:27 +0100

    the jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - Function that uses the junp search algorithm
+ * @array: Array to be searched
+ * @size: Size of the array
+ * @value: Value to be searched for
+ * Return: Return -1 on fail
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int len = (int)size;
+	int a = 0;
+	int b = sqrt(len);
+	int i, j;
+
+	printf("Value checked array[0] = [0]\n");
+	for (i = 0; i < len; i++)
+	{
+		if (array[b] < value && b < len)
+			printf("Value checked array[%d] = [%d]\n", b, array[b]);
+		for (j = a; j <= b; j++)
+		{
+			if (array[j] == value)
+			{
+				printf("Value found between indexes [%d] and [%d]\n", a, b);
+				while (a <= j)
+				{
+					printf("Value checked array[%d] = [%d]\n", a, array[a]);
+					a++;
+				}
+				return (j);
+			}
+		}
+		if (value > array[b] && a == len - 1)
+		{
+			printf("Value found between indexes [%d] and [%d]\n", a, b);
+			printf("Value checked array[%d] = [%d]\n", a, array[a]);
+		}
+
+		a = b;
+		b += sqrt(len);
+	}
+	return (-1);
+}
